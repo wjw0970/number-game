b0f48029ead9d4aa6cea311e78770577e37c6341
jiawen wang <wjw0970@gmail.com>
2023-11-20 19:56:03 +0800

    Initial Commit

---
+++ number-game/number_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  number_gameApp.swift
+//  number-game
+//
+//  Created by Jiawen Wang on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct number_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
